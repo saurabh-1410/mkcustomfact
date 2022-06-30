175a80630b37973f8239d2281f886b040bf1aa69
kumar <kumar@puppet.com>
2022-06-30 11:29:44 +0800

    added new fact with Facter::Core::Execution.execute command

--- lib/facter/testing.rb
+++ lib/facter/testing.rb
@@ -3,3 +3,10 @@ Facter.add('yahoo') do
     'mytest'
   end
 end
+
+Facter.add(:factertesting) do
+  confine :kernel => 'Linux'
+  setcode do
+    Facter::Core::Execution.execute('ls  -lrt /etc/puppetlabs/puppet/puppet.conf')
+  end
+end
