bfc040f3273b37da3acebb33a197162c2ed62008
kumar <kumar@puppet.com>
2022-08-31 12:25:42 +0800

    added new fact with Facter::Core::Execution.execute command

--- lib/facter/testing.rb
+++ lib/facter/testing.rb
@@ -20,6 +20,7 @@ end
 
 Facter.add('puppetconfstat') do
   setcode do
-    Facter::Core::Execution.execute('TZ=UTC stat -c %z /etc/puppetlabs/puppet/puppet.conf | cut -d"." -f1')
+    #Facter::Core::Execution.execute('TZ=UTC stat -c %z /etc/puppetlabs/puppet/puppet.conf | cut -d"." -f1')
+    Facter::Core::Execution.execute('TZ=UTC','stat -c %z /etc/puppetlabs/puppet/puppet.conf | cut -d"." -f1')
   end
 end
