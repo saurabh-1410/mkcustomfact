4aea0d10c200445f1c34c07097025c7145076983
kumar <kumar@puppet.com>
2022-11-07 17:43:00 +0800

    updated the module

--- plans/request.pp
+++ plans/request.pp
@@ -1,7 +1,7 @@
 plan mkcustomfact::request (
   TargetSpec $targets
 ) {
-  $api_key = lookup('api_key')
+  $api_key = lookup('mkcustomfact::api_key')
   #$result  = run_task('mkcustomfact', $targets)
   $result  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
   return $result
