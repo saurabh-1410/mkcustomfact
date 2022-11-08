f1263c0fd7e2982aef00960328f42d1307c0197d
root <root@ent2019811-3d5cda-0.us-west1-c.c.customer-support-scratchpad.internal>
2022-11-08 06:50:11 +0000

    first commit

--- plans/request.pp
+++ plans/request.pp
@@ -2,7 +2,7 @@ plan mkcustomfact::request (
   TargetSpec $targets
 ) {
   $api_key = lookup('mkcustomfact::api_key')
-  #$result  = run_task('mkcustomfact', $targets)
+  out::message($api_key)
   $result  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
   return $result
 }
