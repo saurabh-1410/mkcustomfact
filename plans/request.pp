ecf4183671e2e8611e41ad747eef9bbeb9b38611
Saurabh D <saurabh.dikshit@puppet.com>
2022-11-18 10:46:36 +0000

    sdsddsdsd

--- plans/request.pp
+++ plans/request.pp
@@ -1,8 +1,8 @@
 plan mkcustomfact::request (
   TargetSpec $targets
 ) {
-  $api_key = lookup('api_key')
-  $second_api_key = lookup('second_key')
+  $api_key = lookup('mkcustomfact::api_key')
+  $second_api_key = lookup('mkcustomfact::second_key')
   out::message($api_key)
   out::message($second_api_key)
 }
