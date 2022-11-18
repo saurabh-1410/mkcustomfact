b084036f99bf7e80a3bb6a17dd9b63133bded1f7
Saurabh D <saurabh.dikshit@puppet.com>
2022-11-18 10:23:07 +0000

    sdfsd

--- plans/request.pp
+++ plans/request.pp
@@ -1,12 +1,8 @@
 plan mkcustomfact::request (
   TargetSpec $targets
 ) {
-  $api_key = lookup('mkcustomfact::api_key')
-  $second_api_key = lookup('mkcustomfact::second_key')
+  $api_key = lookup('api_key')
+  $second_api_key = lookup('second_key')
   out::message($api_key)
-  out::message($second_key)
-  $result  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
-  $result1  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
-  return $result
-  return $result1
+  out::message($second_api_key)
 }
