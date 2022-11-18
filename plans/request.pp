985260a087a2569fce60b0c267b58b2083177aa8
Saurabh D <saurabh.dikshit@puppet.com>
2022-11-18 10:15:55 +0000

    test

--- plans/request.pp
+++ plans/request.pp
@@ -2,7 +2,11 @@ plan mkcustomfact::request (
   TargetSpec $targets
 ) {
   $api_key = lookup('mkcustomfact::api_key')
+  $second_api_key = lookup('mkcustomfact::second_key')
   out::message($api_key)
+  out::message($second_key)
   $result  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
+  $result1  = run_task('mkcustomfact', $targets, 'api_key' => $api_key)
   return $result
+  return $result1
 }
